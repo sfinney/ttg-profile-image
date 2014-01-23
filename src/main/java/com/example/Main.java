a235aad6cd249fd0982c1387b706690b4fba6502
sfinney <sfinney@thetasgroup.com>
2014-01-23 10:40:58 +0000

    Removed README, formatted Main.java and added code to HelloServlet.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,27 +3,19 @@ package com.example;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
-/**
- * 
- * This class launches the web application in an embedded Jetty container.
- * This is the entry point to your application. The Java command that is used for
- * launching should fire this main method.
- *
- */
+/* This class launches the web application in an embedded Jetty container and the Java command that is used for 
+ launching should fire this main method */
 public class Main {
-    
-    /**
-     * @param args
-     */
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
     	
         String directory = "src/main/webapp/";
         
-        //The port that we should run on can be set into an environment variable
-        //Look for that variable and default to 8080 if it isn't there.
+        // The port that should be used is set to an environment variable or defaults to 8080 if it's unavailable
         String port = System.getenv("PORT");
         
         if (port == null || port.isEmpty()) {
+        	
             port = "8080";
         }
 
@@ -31,20 +23,14 @@ public class Main {
         WebAppContext root = new WebAppContext();
 
         root.setContextPath("/");
-        root.setDescriptor(directory +"/WEB-INF/web.xml");
+        root.setDescriptor(directory + "/WEB-INF/web.xml");
         root.setResourceBase(directory);
         
-        //Parent loader priority is a class loader setting that Jetty accepts.
-        //By default Jetty will behave like most web containers in that it will
-        //allow your application to replace non-server libraries that are part of the
-        //container. Setting parent loader priority to true changes this behavior.
-        //Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
+        // Allows application to replace non-server libraries that are part of the container
         root.setParentLoaderPriority(true);
         
-        server.setHandler(root);
-        
+        server.setHandler(root);        
         server.start();
         server.join();   
     }
-
 }
